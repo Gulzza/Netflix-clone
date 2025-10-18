0ab5e6259ffcc59de377d4a9463900b1e4704b4e
Gulzza <mursakanovag@gmail.com>
2025-10-17 21:16:32 -0400

    Update VolumeControllers.tsx

--- src/components/watch/VolumeControllers.tsx
+++ src/components/watch/VolumeControllers.tsx
@@ -1,5 +1,4 @@
 import { Stack } from "@mui/material";
-import Slider from "@mui/material/Slider";
 import { styled } from "@mui/material/styles";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 import VolumeOffIcon from "@mui/icons-material/VolumeOff";
