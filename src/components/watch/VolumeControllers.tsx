b44e42fac38145dc9f0a3e9e16ff0e82ec37f168
Gulzza <mursakanovag@gmail.com>
2025-10-17 21:03:21 -0400

    Update VolumeControllers.tsx

--- src/components/watch/VolumeControllers.tsx
+++ src/components/watch/VolumeControllers.tsx
@@ -5,7 +5,7 @@ import { SliderUnstyledOwnProps } from "@mui/base/SliderUnstyled";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 import VolumeOffIcon from "@mui/icons-material/VolumeOff";
 import PlayerControlButton from "./PlayerControlButton";
-import {SliderUnstyled} from "@mui/base/SliderUnstyled";
+import {SliderUnstyledOwnProps} from "@mui/base";
 
 const StyledSlider = styled(Slider)({
   height: 5,
